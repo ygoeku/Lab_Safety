1187875a7b7d8df7ac139ff2c3d44de92ba50504
Elena <avkovele@students.zhaw.ch>
2025-04-27 08:45:13 +0200

    .

--- pages/2_Laborsicherheit.py
+++ pages/2_Laborsicherheit.py
@@ -7,16 +7,20 @@ desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
 st.title("Sicherheitszeichen")
 
 # Funktion zum sicheren Laden
-def load_image(filename):
+def load_image(filename, beschreibung):
     full_path = os.path.join(desktop_path, filename)
     if os.path.exists(full_path):
-        st.image(full_path, caption=filename, width=200)
+        st.image(full_path, caption=beschreibung, width=200)
+        link = st.text_input(f"Link für {beschreibung}:", f"https://dein-link-zu-{beschreibung.replace(' ', '_').lower()}.de")
+        if st.button(f"Mehr Infos zu {beschreibung}"):
+            st.markdown(f"[Hier klicken]({link})")
     else:
         st.error(f"Datei nicht gefunden: {filename}")
 
 # Bilder laden
-load_image("handschuhe.png")
-load_image("laborbrille.gif")
-load_image("laborkittel.png")
-load_image("notfall ausgang.jpg")
+load_image("handschuhe.jpg", "Handschuhe tragen")
+load_image("laborbrille.gif", "Schutzbrille tragen")
+load_image("laborkittel.png", "Laborkittel tragen")
+load_image("notfall ausgang.jpg", "Notausgang")
+
 
