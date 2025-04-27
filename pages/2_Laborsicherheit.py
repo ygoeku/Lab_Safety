405ff323ecd525e2cddf4d802067b0bcd2e5a484
Elena <avkovele@students.zhaw.ch>
2025-04-27 17:04:51 +0200

    nochmal

--- pages/2_Laborsicherheit.py
+++ pages/2_Laborsicherheit.py
@@ -1,15 +1,13 @@
 import streamlit as st
-import os
 from PIL import Image
 
 # Funktion zum Laden und Anzeigen eines Bildes mit Expander für mehr Infos
 def load_image_with_info(filename, beschreibung, mehr_info):
     try:
-        path = os.path.join(filename)
-        if os.path.exists(path):
-            # Bild laden und auf feste Größe anpassen
-            img = Image.open(path)
-            fixed_size = (300, 300)  # Beispiel: 300x300 Pixel
+        # Pfad ohne "pages" direkt benutzen!
+        if os.path.exists(filename):
+            img = Image.open(filename)
+            fixed_size = (300, 300)  # Beispiel: 300x300 Pixel für gleiche Größe
             img = img.resize(fixed_size)
 
             st.image(img, caption=beschreibung)
@@ -33,10 +31,9 @@ bild_liste = [
     ("notruftelefon_2.jpg", "Notruftelefon", "Im Notfall kannst du hier schnell Hilfe rufen. Notrufnummern sollten gut sichtbar sein.")
 ]
 
-# Anzahl der Bilder pro Reihe
+# Bilder automatisch in 2 Spalten anzeigen
 bilder_pro_reihe = 2
 
-# Bilder automatisch anzeigen
 for i in range(0, len(bild_liste), bilder_pro_reihe):
     spalten = st.columns(bilder_pro_reihe)
     for spalte, (filename, beschreibung, mehr_info) in zip(spalten, bild_liste[i:i+bilder_pro_reihe]):
